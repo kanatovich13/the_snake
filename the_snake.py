40e361ff72281e5affa404aaf80c3d86f13aed74
Yelzhan Beisenkul <beisenkulelzhan03@gmail.com>
2026-04-07 21:05:57 +0500

    Ready for review

--- the_snake.py
+++ the_snake.py
@@ -1,7 +1,9 @@
 import pygame
+
 from random import choice, randint
 
 # Константы для размеров поля и сетки:
+
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
